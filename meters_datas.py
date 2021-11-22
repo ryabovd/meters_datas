c0ff7687675501c20be18f1caa8c166d5b7893fc
Dmitry Ryabov <ryabovd@outlook.com>
2021-11-22 12:13:38 +0700

    Work on update data func

--- meters_datas.py
+++ meters_datas.py
@@ -12,7 +12,12 @@ def main():
     print(meters_data_for_print(filename))
     choice = menu()
     if choice == '1':
-        update_meter_data()
+        update_meter_data(filename)
+    elif choice == '2':
+        text = meters_data_for_print(filename)
+        send_notification(text)
+    elif choice == '3':
+        pass
     else:
             while choice != '1':
                 choice = menu()   
@@ -28,17 +33,36 @@ def read_cvs(filename):
     print('Данные прочитаны из файла')
     return data_list
 
+def read_list(list, element):
+    '''Func get list and return n-element string'''
+    return list[element]
+
 def menu():
-    print("МЕНЮ\n\n1 - ОБНОВИТЬ показания счетчика")
+    print("МЕНЮ\n\n1 - ОБНОВИТЬ показания\n2 - ОТПРАВИТЬ показания по email\n3 - ДОБАВИТЬ НОВЫЙ счетчик")
     return input()
 
-def update_meter_data():
+def update_meter_data(filename):
     print('Будем обновлять показания счетчика')
+    meter_number = input('Введите последние цифры номера счетчика ')
+    check_number = check_number_meter(meter_number, filename)
+
+def check_number_meter(number, filename):
+    main_list = read_cvs(filename)
+    for number_line in range(len(main_list)):
+        print(number_line)
+        list = read_list(main_list[number_line], 0)
+        print(list.split(' '))
+        print(list.split(' ')[1])
+        if list.split(' ')[1].endswith(number):
+            return print('Такой счетчик есть'), print(number_line)
+        else:
+            print('Такого счетчика нет')
+            continue
 
 def date_today():
     '''Func that returned today date'''
     today = datetime.date.today()
-    print('Сегодняшняя дата ' + str(today))
+    print('Сегодня ' + str(today))
     return today
 
 def meters_data_for_print(filename):
@@ -59,9 +83,10 @@ def send_notification(text):
     mail_lib.login(sender, sender_password)
     for to_item in email:
         msg = 'From: %s\r\nTo: %s\r\nContent-Type: text/plain; charset="utf-8"\r\nSubject: %s\r\n\r\n' % (
-            sender, to_item, 'Новости арбитражного суда Республики Хакасия на {}'.format(date_today()))
+            sender, to_item, 'Показания приборов учета на {}'.format(date_today()))
         msg += text
         mail_lib.sendmail(sender, to_item, msg.encode('utf8'))
+    print('Данные отправлены')
     mail_lib.quit()
 
 if __name__ == "__main__":
@@ -73,5 +98,7 @@ personal account number
 meter number
 meter data (sep. is - ',')
 date of update data
-date of send's dat)
+date of send's data)
 """
+
+# Write func for check number meter
