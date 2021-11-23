32ce60029f9dcae12b5832c3ed0eeb4cb602c0ef
Dmitry Ryabov <ryabovd@outlook.com>
2021-11-23 22:34:05 +0700

    Script sends email.

--- meters_datas.py
+++ meters_datas.py
@@ -3,27 +3,42 @@ import datetime
 import smtplib
 import json
 
+"""
+meters_datas.csv structure (from left to right):
+personal account number
+meter number
+meter data (sep. is - ',')
+date of update data
+date of send's data)
+"""
 
 def main():
     filename = "meters_datas"
     datas = read_cvs(filename)
-    print(datas)
-    print(date_today())
+    #print(datas)
+    print("Сегодня" + str(date_today()) + "\n")
     #print(meters_data_for_print(filename))
-    choice = menu()
-    if choice == '1':
-        index, line = update_meter_data(filename)
-        write_line_csv(index, line, filename)
-
-        choice == menu()
-    elif choice == '2':
-        text = meters_data_for_print(filename)
-        send_notification(text)
-    elif choice == '3':
-        print('Func to add new meter')
-    else:
-            while choice not in '123':
-                choice = menu()   
+    while True:
+        choice = menu()
+        if choice == '1':
+            index, line = update_meter_data(filename)
+            data, filename_with_date = write_line_csv(index, line, filename)
+            write_csv(filename, data)
+            write_csv(filename_with_date, data)
+            continue
+        elif choice == '2':
+            #print('Func that send email')
+            text = meters_data_for_print(filename)
+            #print(text)
+            send_notification(text)
+            continue
+        elif choice == '3':
+            print('Func to add new meter')
+        elif choice == '0':
+            print("THE END")
+            break
+        else:
+                continue   
 
 def read_cvs(filename):
     '''Func that reads csv file (delimiter is '\n') and returns a list of data'''
@@ -42,7 +57,11 @@ def read_list(list, index):
     return list[index]
 
 def menu():
-    print("МЕНЮ\n\n1 - ОБНОВИТЬ показания\n2 - ОТПРАВИТЬ показания по email\n3 - ДОБАВИТЬ НОВЫЙ счетчик")
+    print("МЕНЮ\n\n")
+    print("1 - ОБНОВИТЬ показания")
+    print("2 - ОТПРАВИТЬ показания по email")
+    print("3 - ДОБАВИТЬ НОВЫЙ счетчик")
+    print("0 - ВЫХОД")
     return input()
 
 def update_meter_data(filename):
@@ -51,12 +70,12 @@ def update_meter_data(filename):
     index, line = check_number_meter(meter_number, filename)
     #data = read_cvs(filename)
     #data[index] = line
-    print('Нужно обновить строку')
-    print(line)
+    print(f'Обновляем данные счетчика {line[1]} \nПоследние показания {line[2]}')
+    #print(line)
     meter_data = input('Введите показания счетчика с запятой: ')
     #Обновляем данные счетчика
     line[2], line[3] = meter_data, str(date_today())
-    print(line)
+    #print(line)
     return index, line
 
 def check_number_meter(number, filename):
@@ -64,10 +83,10 @@ def check_number_meter(number, filename):
     for number_line in range(len(main_list)):
         #print(number_line)
         list = read_list(main_list[number_line], 0)
-        print(list.split(' '))
-        print(list.split(' ')[1])
+        #print(list.split(' '))
+        #print(list.split(' ')[1])
         if list.split(' ')[1].endswith(number):
-            print('Такой счетчик есть'), print(number_line)
+            #print('Такой счетчик есть'), print(number_line)
             return number_line, list.split(' ')
         else:
             print('Такого счетчика нет')
@@ -76,33 +95,35 @@ def check_number_meter(number, filename):
 def date_today():
     '''Func that returned today date'''
     today = datetime.date.today()
-    print('Сегодня ' + str(today))
     return today
 
 def write_line_csv(index, line, filename):
     '''Func that write csv file with meters datas'''
     data = read_cvs(filename)
     data[index] = [(' ').join(line)]
-    print(data)
+    #print(data)
     today_date = str(date_today())
-    filename = today_date + ' meters_datas' + '.csv'
+    filename_with_date = today_date + ' meters_datas'
     #meters_datas_list = data.split('\n')
-    with open(file=filename, mode="w", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile, delimiter=' ')
-        for list in data:
-            line = list[0].split()
-            writer.writerow(line)
+    return data, filename_with_date
+    
+def write_csv(filename, data):
+    filename += '.csv'
+    with open(file=filename, mode="w", encoding="utf-8", newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerows(data)
 
 def meters_data_for_print(filename):
     '''Function that returns a text with datas for print or email'''
     meters_data_for_print = ""
     for list in read_cvs(filename):
-        meters_data_for_print += list[0] + '\n'
+        meters_data_for_print += (' ').join(list[0].split()[:3]) + '\n'
     print('Данные подготовлены для записи/отправки')
     return meters_data_for_print
 
 def send_notification(text):
     email = ['ryabovd@outlook.com']
+    #email = ['pokazvodokanal@mail.ru']
     with open('settings.json', 'r', encoding='utf-8') as file:
         settings = json.load(file)
         sender = settings["sender"]
@@ -114,19 +135,9 @@ def send_notification(text):
             sender, to_item, 'Показания приборов учета на {}'.format(date_today()))
         msg += text
         mail_lib.sendmail(sender, to_item, msg.encode('utf8'))
-    print('Данные отправлены')
+    print('Данные отправлены на ' + email[0].upper())
+    # Дописать функцию изменения строки с датой отправки
     mail_lib.quit()
 
 if __name__ == "__main__":
     main()
-
-"""
-meters_datas.csv structure (from left to right):
-personal account number
-meter number
-meter data (sep. is - ',')
-date of update data
-date of send's data)
-"""
-
-# Write func for check number meter
